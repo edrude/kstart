4c87a040b001085190fb920cff0627440b623aa2
Ed Rude <erude1@umbc.edu>
2025-01-28 12:20:25 -0500

    attempt to add systemd support

--- commands/framework.c
+++ commands/framework.c
@@ -33,6 +33,9 @@
 #include <portable/system.h>
 
 #include <errno.h>
+#ifdef HAVE_SYSTEMD
+#include <systemd/sd-daemon.h>
+#endif
 #ifdef HAVE_LIBKEYUTILS
 #    include <keyutils.h>
 #endif
@@ -443,6 +446,23 @@ run_framework(krb5_context ctx, struct config *config)
         if (config->childfile != NULL)
             write_pidfile(config->childfile, child);
         config->child = child;
+    #ifdef HAVE_SYSTEMD
+    /* After spawning the child, notify systemd that MAINPID should be the child's PID. */
+    {
+        char notify_buf[64];
+        /* Format: "MAINPID=<PID>" */
+        snprintf(notify_buf, sizeof(notify_buf), "MAINPID=%ld", (long) child);
+
+        int ret = sd_notify(/* unset_environment= */ 0, notify_buf);
+        if (ret < 0) {
+            warn("sd_notify(MAINPID) failed: %s", strerror(-ret));
+        } else if (ret == 0) {
+            /* 0 means NOTIFY_SOCKET wasn't set or not accessible. */
+            warn("sd_notify(MAINPID) was not sent (NOTIFY_SOCKET not set?)");
+        }
+        /* ret > 0 => successfully queued the notification. */
+    }
+    #endif
     }
 
     /* Loop if we're running as a daemon. */
